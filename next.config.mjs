49babfb9697c56d1690123a244f68b46494e7324
ibelick <julien.thibeaut@gmail.com>
2024-07-17 11:50:32 +0200

    feat: add remarkGfm to support tables in mdx

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,18 @@
-import nextMDX from "@next/mdx";
-
-const withMDX = nextMDX();
+import remarkGfm from "remark-gfm";
+import createMDX from "@next/mdx";
+import { remarkCodeHike } from "@code-hike/mdx";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+  reactStrictMode: true,
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
 };
+
+const withMDX = createMDX({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [remarkGfm, [remarkCodeHike, { theme: "css-variables" }]],
+  },
+});
 
 export default withMDX(nextConfig);
