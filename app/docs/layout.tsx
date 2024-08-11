e55836bc65f282405088c86c7e92c71c2767a83c
Malki Abderrahmane <malki.abdurrahmane@gmail.com>
2024-08-11 17:46:56 +0100

    fix(aside): specify a top value for sticky position

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -117,7 +117,7 @@ function NavigationDesktop() {
   const pathname = usePathname();
 
   return (
-    <aside className='sticky hidden h-[calc(100dvh-theme(spacing.16))] w-[220px] shrink-0 pt-8 md:block lg:pt-12'>
+    <aside className='sticky top-14 hidden h-[calc(100dvh-theme(spacing.16))] w-[220px] shrink-0 pt-8 md:block lg:pt-12'>
       <ScrollArea>
         <nav>
           <ul role='list' className='h-full'>
