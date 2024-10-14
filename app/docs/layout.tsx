60caa96a48e997543922cb08170747e08f14831a
ibelick <julien.thibeaut@gmail.com>
2024-10-14 20:28:55 +0200

    feat: update wording

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -8,6 +8,7 @@ import Link from 'next/link';
 import { usePathname, useRouter } from 'next/navigation';
 import React from 'react';
 import { TableOfContents } from '@/components/website/table-of-contents';
+import { MPLogo } from '@/components/website/icons/motion-primitives-logo';
 
 type NavigationItem = {
   name: string;
@@ -127,12 +128,15 @@ function Header() {
   return (
     <header className='sticky top-0 z-10 flex h-16 items-center justify-center border-b border-zinc-950/10 bg-white px-6 py-5 dark:border-white/10 dark:bg-zinc-950'>
       <div className='mx-auto flex w-full items-center justify-between md:max-w-7xl'>
-        <a href='/' className='relative flex items-center'>
-          <div className='text-zinc-950 dark:text-white'>motion-primitives</div>
-          <span className='mb-4 ml-2 select-none rounded-full bg-zinc-800 px-2 py-0.5 text-xs text-zinc-50'>
+        <Link href='/docs' className='relative flex items-center space-x-2'>
+          <MPLogo className='h-6 w-auto' />
+          <div className='text-sm font-medium text-zinc-950 dark:text-white'>
+            motion-primitives
+          </div>
+          <span className='mb-4 ml-2 select-none rounded bg-zinc-800 px-1.5 py-0.5 text-[10px] font-medium text-zinc-50'>
             beta
           </span>
-        </a>
+        </Link>
         <nav className='flex items-center gap-2'>
           <a
             href='https://twitter.com/Ibelick'
