1c38a9bfacf6dbee98bbee1075cd986443041b55
bhataasim1 <bhataasim700@gmail.com>
2024-10-17 11:26:52 +0530

    feat: add SkeletonBasic1 component with examples

---
+++ components/examples/skeleton-basic-1.tsx
@@ -0,0 +1,13 @@
+import { Skeleton } from "../core/skeleton";
+
+export function SkeletonBasic1() {
+  return (
+    <div className="flex items-center space-x-4">
+      <Skeleton className="size-12 rounded-full" />
+      <div className="space-y-2">
+        <Skeleton className="h-4 w-[250px]" />
+        <Skeleton className="h-4 w-[200px]" />
+      </div>
+    </div>
+  )
+}
