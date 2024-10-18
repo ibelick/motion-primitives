1ca8f97e2644608a0ffcf21bcc085b4157ed48b5
ibelick <julien.thibeaut@gmail.com>
2024-10-18 11:04:04 +0200

    feat: add dark TextMorphInput

--- components/examples/text-morph-input.tsx
+++ components/examples/text-morph-input.tsx
@@ -1,18 +1,20 @@
 'use client';
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { TextMorph } from '../core/text-morph';
 
 export function TextMorphInput() {
   const [text, setText] = useState('Craft');
 
   return (
-    <div className='flex flex-col items-center justify-end space-y-4'>
-      <TextMorph className='text-5xl font-[450] text-black'>{text}</TextMorph>
+    <div className='flex flex-col items-center justify-end space-y-12'>
+      <TextMorph className='text-5xl font-medium text-black dark:text-white'>
+        {text}
+      </TextMorph>
       <input
         type='text'
         value={text}
         onChange={(e) => setText(e.target.value)}
-        className='h-9 w-full rounded-lg border border-zinc-950/10 bg-transparent p-2 text-zinc-900 placeholder-zinc-500 focus:outline-none'
+        className='h-9 w-full rounded-lg border border-zinc-950/10 bg-transparent p-2 text-zinc-900 placeholder-zinc-500 focus:outline-none dark:border-zinc-50/10 dark:text-white dark:placeholder-zinc-400'
         placeholder='Type your text here'
       />
     </div>
