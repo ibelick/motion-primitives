34d108eefb4a6d3c013f56847e5eb3f31b1d9ec2
ibelick <julien.thibeaut@gmail.com>
2024-10-28 20:23:43 +0100

    feat: add PH banner

--- components/website/launch-banner.tsx
+++ components/website/launch-banner.tsx
@@ -2,7 +2,7 @@ export default function LaunchBanner() {
   return (
     <a
       target='_blank'
-      className='inline-flex w-full flex-col items-center justify-center gap-2 bg-blue-800 px-4 py-2 text-xs font-medium leading-normal text-white md:text-sm'
+      className='inline-flex w-full flex-col items-center justify-center gap-2 bg-[#0D74CE] px-4 py-2 text-xs font-medium leading-normal text-white dark:bg-[#003362] md:text-sm'
       href='https://pro.motion-primitives.com'
     >
       <span>
