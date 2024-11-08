95ba33dbf80748f7ecefed425490303541bf2d85
afpedreros <felipe.pedreros94@gmail.com>
2024-11-08 14:36:53 -0500

    feat: add Tiltable component with examples and documentation

---
+++ components/core/tiltable.tsx
@@ -0,0 +1,97 @@
+'use client';
+
+import React, { useRef } from 'react';
+import {
+  motion,
+  useMotionTemplate,
+  useMotionValue,
+  useSpring,
+  useTransform,
+  MotionStyle,
+  Transition,
+} from 'framer-motion';
+
+type TiltProps = {
+  children: React.ReactNode;
+  className?: string;
+  transition?: Transition;
+  style?: MotionStyle;
+  rotationFactor?: number;
+};
+
+export function Tilt({
+  children,
+  className,
+  transition,
+  style,
+  rotationFactor = 15,
+}: TiltProps) {
+  const ref = useRef<HTMLDivElement>(null);
+
+  const x = useMotionValue(0);
+  const y = useMotionValue(0);
+
+  const xSpring = useSpring(x);
+  const ySpring = useSpring(y);
+
+  const rotateX = useTransform(
+    ySpring,
+    [-0.5, 0.5],
+    [rotationFactor, -rotationFactor]
+  );
+  const rotateY = useTransform(
+    xSpring,
+    [-0.5, 0.5],
+    [-rotationFactor, rotationFactor]
+  );
+
+  const transform = useMotionTemplate`perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+  const scale = useSpring(1);
+
+  const BASE_TRANSITION: Transition = {
+    type: 'spring',
+    stiffness: 900,
+    damping: 80,
+    mass: 5,
+  };
+
+  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+    if (!ref.current) return;
+
+    const rect = ref.current.getBoundingClientRect();
+    const width = rect.width;
+    const height = rect.height;
+    const mouseX = e.clientX - rect.left;
+    const mouseY = e.clientY - rect.top;
+
+    const xPos = mouseX / width - 0.5;
+    const yPos = mouseY / height - 0.5;
+
+    x.set(xPos);
+    y.set(yPos);
+  };
+
+  const handleMouseLeave = () => {
+    x.set(0);
+    y.set(0);
+    scale.set(1);
+  };
+
+  return (
+    <motion.div
+      ref={ref}
+      className={className}
+      style={{
+        transformStyle: 'preserve-3d',
+        ...style,
+        transform,
+        scale,
+      }}
+      transition={transition ?? BASE_TRANSITION}
+      onMouseMove={handleMouseMove}
+      onMouseLeave={handleMouseLeave}
+    >
+      {children}
+    </motion.div>
+  );
+}
