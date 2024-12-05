a1c1d0abc53450e551872f26e62d40749f769366
ibelick <julien.thibeaut@gmail.com>
2024-12-05 21:11:33 +0100

    feat: add usePreventScroll, prevent scroll on iOS

--- components/core/dialog.tsx
+++ components/core/dialog.tsx
@@ -5,7 +5,7 @@ import { cn } from '@/lib/utils';
 import { useId } from 'react';
 import { createPortal } from 'react-dom';
 import { X } from 'lucide-react';
-
+import { usePreventScroll } from '@/hooks/usePreventScroll';
 const DialogContext = createContext<{
   isOpen: boolean;
   setIsOpen: (open: boolean) => void;
@@ -60,6 +60,11 @@ function Dialog({
   );
   const dialogRef = useRef<HTMLDialogElement>(null);
   const isOpen = open !== undefined ? open : uncontrolledOpen;
+
+  // prevent scroll when dialog is open on iOS
+  usePreventScroll({
+    isDisabled: !isOpen,
+  });
 
   const setIsOpen = React.useCallback(
     (value: boolean) => {
