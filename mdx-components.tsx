4fcf2a345534c02a1665342b4e3a2b26ad5f3dd1
ibelick <julien.thibeaut@gmail.com>
2024-12-06 19:14:49 +0100

    feat: ui update table

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -72,29 +72,57 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
       className,
       ...props
     }: React.ComponentProps<typeof TabsList>) => (
-      <TabsList
-        className={cn(
-          className
-        )}
-        {...props}
-      />
+      <TabsList className={cn(className)} {...props} />
     ),
     TabsTrigger: ({
       className,
       ...props
     }: React.ComponentProps<typeof TabsTrigger>) => (
-      <TabsTrigger
-        className={cn(
-          className
-        )}
-        {...props}
-      />
+      <TabsTrigger className={cn(className)} {...props} />
     ),
     TabsContent: ({
       className,
       ...props
     }: React.ComponentProps<typeof TabsContent>) => (
       <TabsContent className={cn(className)} {...props} />
+    ),
+    table: ({ className, ...props }: React.ComponentProps<'table'>) => (
+      <div className='not-prose relative w-full table-auto overflow-auto rounded-lg border border-zinc-200 text-sm dark:border-zinc-800'>
+        <table className={cn('w-full', className)} {...props} />
+      </div>
+    ),
+    thead: ({ className, ...props }: React.ComponentProps<'thead'>) => (
+      <thead
+        className={cn(
+          'bg-zinc-100 text-zinc-900 dark:bg-zinc-900 dark:text-zinc-100',
+          className
+        )}
+        {...props}
+      />
+    ),
+    tbody: ({ className, ...props }: React.ComponentProps<'tbody'>) => (
+      <tbody
+        className={cn(
+          'divide-y divide-zinc-200 dark:divide-y dark:divide-zinc-600',
+          className
+        )}
+        {...props}
+      />
+    ),
+    tr: ({ className, ...props }: React.ComponentProps<'tr'>) => (
+      <tr className={cn('h-10', className)} {...props} />
+    ),
+    th: ({ className, ...props }: React.ComponentProps<'th'>) => (
+      <th
+        className={cn('px-4 pb-0 text-left align-middle font-[450]', className)}
+        {...props}
+      />
+    ),
+    td: ({ className, ...props }: React.ComponentProps<'td'>) => (
+      <td
+        className={cn('px-4 py-2 text-left align-middle', className)}
+        {...props}
+      />
     ),
   };
 }
