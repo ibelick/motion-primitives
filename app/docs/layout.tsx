351707cc728f58183131a8aa7f20edcca7daa1aa
Julien Thibeaut <julien.thibeaut@gmail.com>
2024-12-10 16:08:24 +0100

    feat: doc UI update, add shadcn manual install (#88)
    * feat: add steps
    
    * feat: add mdx tabs
    
    * docs: add installation tabs for each components
    
    * feat: ui update table
    
    * feat: add layout underline tab
    
    * feat: add underline sidebar
    
    * feat: add manual shadcn install

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -7,6 +7,7 @@ import React, { useEffect, useRef } from 'react';
 import { TableOfContents } from '@/components/website/table-of-contents';
 import LaunchBanner from '@/components/website/launch-banner';
 import { Header } from '@/components/website/header';
+import { motion } from 'motion/react';
 
 type NavigationItem = {
   name: string;
@@ -182,16 +183,16 @@ function NavigationDesktop() {
     <aside className='sticky top-14 hidden h-[calc(100dvh-theme(spacing.16))] w-[220px] shrink-0 pt-8 md:block lg:pt-12'>
       <ScrollArea className='h-full w-full'>
         <nav>
-          <ul role='list' className='h-full'>
+          <ul role='list' className='h-full [&>li:not(:first-child)>div]:pt-6'>
             {NAVIGATION.map((item, index) => {
               return (
-                <li className='mb-6' key={`${item.name}-${index}`}>
-                  <div className='text-sm/6 font-[450] text-zinc-950 dark:text-white'>
+                <li key={`${item.name}-${index}`}>
+                  <div className='relative z-10 w-11/12 bg-white pb-4 text-sm/6 font-[450] text-zinc-950 dark:bg-zinc-950 dark:text-white'>
                     {item.name}
                   </div>
                   <ul
                     role='list'
-                    className='mt-4 space-y-3.5 border-l border-zinc-200 dark:border-zinc-800'
+                    className='space-y-3.5 border-l border-zinc-200 dark:border-zinc-800'
                   >
                     {item.children.map((child) => {
                       const isActive = pathname === child.href;
@@ -200,20 +201,32 @@ function NavigationDesktop() {
                         <li key={child.href} ref={isActive ? activeRef : null}>
                           <Link
                             className={cn(
-                              'relative inline-flex items-center space-x-1 pl-4 text-sm font-normal text-zinc-700 hover:text-zinc-950 dark:text-zinc-400 dark:hover:text-white',
-                              isActive &&
-                                'text-zinc-950 before:absolute before:inset-y-0 before:left-[-1.5px] before:w-[2px] before:rounded-full before:bg-zinc-950 dark:text-white dark:before:bg-white'
+                              'relative inline-flex items-center pl-4 text-sm font-normal text-zinc-700 hover:text-zinc-950 dark:text-zinc-400 dark:hover:text-white',
+                              isActive && 'text-zinc-950'
                             )}
                             href={child.href}
                           >
+                            {isActive && (
+                              <motion.div
+                                layout
+                                className='absolute -left-[1px] top-0 -z-[1] h-full w-0.5 rounded-[4px] bg-zinc-950 dark:bg-white'
+                                transition={{
+                                  type: 'spring',
+                                  stiffness: 26.7,
+                                  damping: 4.1,
+                                  mass: 0.2,
+                                }}
+                                layoutId='underline-sidebar'
+                              />
+                            )}
                             <span>{child.name}</span>
                             {child?.isNew && (
-                              <span className='whitespace-nowrap rounded-lg bg-emerald-100 px-2 text-[10px] font-semibold text-emerald-800 dark:bg-emerald-950 dark:text-emerald-50'>
+                              <span className='ml-2 whitespace-nowrap rounded-lg bg-emerald-100 px-2 text-[10px] font-semibold text-emerald-800 dark:bg-emerald-950 dark:text-emerald-50'>
                                 New
                               </span>
                             )}
                             {child?.isUpdated && (
-                              <span className='whitespace-nowrap rounded-lg bg-amber-100 px-2 text-[10px] font-semibold text-amber-800 dark:bg-amber-950 dark:text-amber-50'>
+                              <span className='ml-2 whitespace-nowrap rounded-lg bg-amber-100 px-2 text-[10px] font-semibold text-amber-800 dark:bg-amber-950 dark:text-amber-50'>
                                 Updated
                               </span>
                             )}
@@ -280,7 +293,7 @@ export default function ComponentLayout({
           <div className='mx-auto flex w-full flex-col items-start md:flex-row md:space-x-12'>
             <NavigationDesktop />
             <NavigationMobile />
-            <main className='prose prose-zinc min-w-0 max-w-full flex-1 pb-16 pt-8 dark:prose-invert prose-h1:scroll-m-20 prose-h1:text-2xl prose-h1:font-semibold prose-h2:scroll-m-20 prose-h2:text-xl prose-h2:font-medium prose-h3:scroll-m-20 prose-h3:text-base prose-h3:font-medium prose-h4:scroll-m-20 prose-h5:scroll-m-20 prose-h6:scroll-m-20 prose-strong:font-medium prose-table:block prose-table:overflow-y-auto lg:pt-12 xl:max-w-2xl'>
+            <main className='prose prose-zinc min-w-0 max-w-full flex-1 pb-16 pt-8 dark:prose-invert prose-h1:scroll-m-20 prose-h1:text-2xl prose-h1:font-semibold prose-h2:mt-12 prose-h2:scroll-m-20 prose-h2:text-xl prose-h2:font-medium prose-h3:scroll-m-20 prose-h3:text-base prose-h3:font-medium prose-h4:scroll-m-20 prose-h5:scroll-m-20 prose-h6:scroll-m-20 prose-strong:font-medium prose-table:block prose-table:overflow-y-auto lg:pt-12 xl:max-w-2xl'>
               {children}
             </main>
             <aside className='sticky top-14 hidden h-[calc(100dvh-theme(spacing.16))] w-[220px] shrink-0 pt-8 lg:pt-12 xl:block'>
--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,5 +1,11 @@
 import type { MDXComponents } from 'mdx/types';
 import { cn } from '@/lib/utils';
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from './components/website/tabs';
 
 const generateId = (text: string) => {
   return text
@@ -43,5 +49,80 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
         </h4>
       );
     },
+    Step: ({ className, children, ...props }: React.ComponentProps<'h3'>) => (
+      <h3
+        id={generateId(children?.toString() || '')}
+        className={cn('step', className)}
+        data-heading='3'
+        {...props}
+      >
+        {children}
+      </h3>
+    ),
+    Steps: ({ ...props }) => (
+      <div
+        className='steps mb-12 ml-4 border-l pl-8 [counter-reset:step]'
+        {...props}
+      />
+    ),
+    Tabs: ({ className, ...props }: React.ComponentProps<typeof Tabs>) => (
+      <Tabs className={cn('relative mt-6 w-full', className)} {...props} />
+    ),
+    TabsList: ({
+      className,
+      ...props
+    }: React.ComponentProps<typeof TabsList>) => (
+      <TabsList className={cn(className)} {...props} />
+    ),
+    TabsTrigger: ({
+      className,
+      ...props
+    }: React.ComponentProps<typeof TabsTrigger>) => (
+      <TabsTrigger className={cn(className)} {...props} />
+    ),
+    TabsContent: ({
+      className,
+      ...props
+    }: React.ComponentProps<typeof TabsContent>) => (
+      <TabsContent className={cn(className)} {...props} />
+    ),
+    table: ({ className, ...props }: React.ComponentProps<'table'>) => (
+      <div className='not-prose relative w-full table-auto overflow-auto rounded-lg border border-zinc-200 text-sm dark:border-zinc-800'>
+        <table className={cn('w-full', className)} {...props} />
+      </div>
+    ),
+    thead: ({ className, ...props }: React.ComponentProps<'thead'>) => (
+      <thead
+        className={cn(
+          'bg-zinc-100 text-zinc-900 dark:bg-zinc-900 dark:text-zinc-100',
+          className
+        )}
+        {...props}
+      />
+    ),
+    tbody: ({ className, ...props }: React.ComponentProps<'tbody'>) => (
+      <tbody
+        className={cn(
+          'divide-y divide-zinc-200 dark:divide-y dark:divide-zinc-600',
+          className
+        )}
+        {...props}
+      />
+    ),
+    tr: ({ className, ...props }: React.ComponentProps<'tr'>) => (
+      <tr className={cn('h-10', className)} {...props} />
+    ),
+    th: ({ className, ...props }: React.ComponentProps<'th'>) => (
+      <th
+        className={cn('px-4 pb-0 text-left align-middle font-[450]', className)}
+        {...props}
+      />
+    ),
+    td: ({ className, ...props }: React.ComponentProps<'td'>) => (
+      <td
+        className={cn('px-4 py-2 text-left align-middle', className)}
+        {...props}
+      />
+    ),
   };
 }
