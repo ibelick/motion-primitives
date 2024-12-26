87b355e4f2b0774c1df73f36126e220fd98d41e6
ibelick <julien.thibeaut@gmail.com>
2024-12-26 13:49:28 +0100

    feat: update tw config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,7 @@ const config: Config = {
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './mdx-components.tsx',
   ],
   darkMode: 'class',
   theme: {
