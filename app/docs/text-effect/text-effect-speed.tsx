02d6b61888f53980a66363ab0e43835f8ca491b9
ibelick <julien.thibeaut@gmail.com>
2024-12-30 15:36:27 +0100

    feat: udpate props

--- app/docs/text-effect/text-effect-speed.tsx
+++ app/docs/text-effect/text-effect-speed.tsx
@@ -2,7 +2,7 @@ import { TextEffect } from '@/components/core/text-effect';
 
 export function TextEffectSpeed() {
   return (
-    <TextEffect preset='fade-in-blur' speedReveal={1.1} speedAnimation={0.3}>
+    <TextEffect preset='fade-in-blur' speedReveal={1.1} speedSegment={0.3}>
       Animate your ideas with motion-primitives.
     </TextEffect>
   );
