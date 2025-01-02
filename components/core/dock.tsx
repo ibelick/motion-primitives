b2647359e3f62f2048a818428b8971b1d6011c8c
Yaseen-Ejaz <yaseenejazcaa@gmail.com>
2025-01-01 21:42:03 -0500

    Fixed dock

--- components/core/dock.tsx
+++ components/core/dock.tsx
@@ -18,9 +18,9 @@ import {
   useMemo,
   useRef,
   useState,
+  React
 } from 'react';
 import { cn } from '@/lib/utils';
-import React from 'react';
 
 const DOCK_HEIGHT = 128;
 const DEFAULT_MAGNIFICATION = 80;
@@ -109,7 +109,7 @@ function Dock({
           mouseX.set(Infinity);
         }}
         className={cn(
-          'mx-auto flex w-fit gap-4 rounded-2xl bg-black px-4 dark:bg-neutral-900',
+          'mx-auto flex w-fit gap-4 rounded-2xl bg-gray-50 px-4 dark:bg-neutral-900',
           className
         )}
         style={{ height: panelHeight }}
@@ -172,8 +172,6 @@ function DockItem({ children, className }: DockItemProps) {
   );
 }
 
-
-
 function DockLabel({ children, className, ...rest }: DockLabelProps) {
   const restProps = rest as Record<string, unknown>;
   const isHovered = restProps['isHovered'] as MotionValue<number>;
@@ -196,7 +194,7 @@ function DockLabel({ children, className, ...rest }: DockLabelProps) {
           exit={{ opacity: 0, y: 0 }}
           transition={{ duration: 0.2 }}
           className={cn(
-            'absolute -top-6 left-1/2 w-fit whitespace-pre rounded-md px-2 py-0.5 text-sm text-white dark:border-neutral-900 dark:bg-neutral-800 dark:text-white',
+            'absolute -top-6 left-1/2 w-fit whitespace-pre rounded-md border border-gray-200 bg-gray-100 px-2 py-0.5 text-xs text-neutral-700 dark:border-neutral-900 dark:bg-neutral-800 dark:text-white',
             className
           )}
           role='tooltip'
