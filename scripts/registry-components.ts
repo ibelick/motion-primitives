c612a9aa31a1a4812f8658b5842339991435cfc4
ibelick <julien.thibeaut@gmail.com>
2025-02-14 16:36:03 +0100

    feat: run registry

--- scripts/registry-components.ts
+++ scripts/registry-components.ts
@@ -143,6 +143,12 @@ export const components: ComponentDefinition[] = [
     dependencies: ['motion'],
   },
   {
+    name: 'morphing-popover',
+    path: path.join(__dirname, '../components/core/morphing-popover.tsx'),
+    registryDependencies: [],
+    dependencies: ['motion'],
+  },
+  {
     name: 'dock',
     path: path.join(__dirname, '../components/core/dock.tsx'),
     registryDependencies: [],
