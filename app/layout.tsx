730f1019686173474b0f265fe7a54e93114ad625
Julien Thibeaut <julien.thibeaut@gmail.com>
2025-06-04 13:55:49 +0200

    feat: update metadata, showcase page (#154)
    * feat: update home metadata
    
    * feat: update components metadata
    
    * feat: update showcase page

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,9 +9,10 @@ const inter = Inter({ subsets: ['latin'] });
 const geistMono = GeistMono;
 
 export const metadata: Metadata = {
-  title: 'Motion-Primitives',
+  title:
+    'Motion-Primitives - UI kit to make beautiful, animated interfaces, faster.',
   description:
-    'UI kit to make beautiful, animated interfaces, faster. Open-source and customizable.',
+    'Motion-Primitives is an open-source UI kit to make beautiful, animated interfaces, faster. Built for React, Next.js, and Tailwind CSS.',
 };
 
 export default function RootLayout({
